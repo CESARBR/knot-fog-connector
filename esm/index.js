2cc74407edaf0d6ec6906957a97f005d2bb7241d
João Neto <joao.alexandre@cesar.org.br>
2018-08-08 14:13:18 -0300

    Setup index to polling update devices

--- esm/index.js
+++ esm/index.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-console */
+// Infrastructure
 import Settings from 'data/Settings';
+import DeviceStore from 'data/DeviceStore';
 import ConnectorFactory from 'network/ConnectorFactory';
 import FogConnector from 'network/FogConnector';
 
+// Domain
+import UpdateDevices from 'interactors/UpdateDevices';
+import LoadDevices from 'interactors/LoadDevices';
+import DevicesService from 'services/DevicesService';
+
 const settings = new Settings();
+const deviceStore = new DeviceStore();
 
 async function main() {
   const fogCredentials = await settings.getFogCredentials();
@@ -11,19 +20,29 @@ async function main() {
   const cloudType = await settings.getCloudType();
 
   try {
+    let fog;
     const cloud = ConnectorFactory.create(cloudType, cloudSettings);
     if (fogCredentials.uuid && fogCredentials.token) {
-      const fog = new FogConnector(
+      fog = new FogConnector(
         fogAddress.host,
         fogAddress.port,
         fogCredentials.uuid,
         fogCredentials.token,
       );
+
       await fog.connect();
       await cloud.start();
     } else {
       throw Error('Missing uuid and token');
     }
+
+    const updateDevices = new UpdateDevices(deviceStore, fog, cloud);
+    const loadDevices = new LoadDevices(deviceStore, cloud);
+    const devicesService = new DevicesService(updateDevices, loadDevices);
+
+    await devicesService.load();
+
+    setInterval(devicesService.update.bind(devicesService), 5000);
   } catch (err) {
     console.error(err);
   }
