610b3d49497e77d2b7ea5cce628f8401d2e9e82a
Ramon Ribeiro <rhpr@cesar.org.br>
2019-02-05 14:02:18 -0300

    Connect to KNoT Cloud
    Change-Id: Idb8b39dac63ba47e9ce04029d288a02fb77adb77

--- src/network/ConnectorFactory.js
+++ src/network/ConnectorFactory.js
@@ -1,6 +1,12 @@
+import KnotCloudConnector from '@cesarbr/knot-fog-connector-knot-cloud';
+import logger from 'util/logger';
+
 class ConnectorFactory {
   static create(type, settings) { // eslint-disable-line no-unused-vars
     switch (type) {
+      case 'KNOT_CLOUD':
+        logger.debug('KNoT Cloud Connector selected');
+        return new KnotCloudConnector(settings);
       default:
         throw Error('Unknown cloud');
     }
