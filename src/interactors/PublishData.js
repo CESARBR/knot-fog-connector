50ef2bfbc6482735f2b0c3e63a0965aef443b0be
Ramon Ribeiro <rhpr@cesar.org.br>
2019-02-11 16:45:53 -0300

    Convert schema from snake_case to camelCase
    Change-Id: I42c719a924475451d4f34146cd3e389e8e5602f3

--- src/interactors/PublishData.js
+++ src/interactors/PublishData.js
@@ -1,10 +1,12 @@
+import convertToCamelCase from 'util/camelCase';
+
 class PublishData {
   constructor(cloudConnector) {
     this.cloudConnector = cloudConnector;
   }
 
   async execute(id, data) {
-    await this.cloudConnector.publishData(id, [data]);
+    await this.cloudConnector.publishData(id, [convertToCamelCase(data)]);
   }
 }
 
