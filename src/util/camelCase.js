f0ebe40f2bd4dc4ca34d3538d5d599f9e6406864
Ramon Ribeiro <rhpr@cesar.org.br>
2019-02-11 16:45:53 -0300

    Convert schema from snake_case to camelCase
    Change-Id: I42c719a924475451d4f34146cd3e389e8e5602f3

---
+++ src/util/camelCase.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+
+export default function convertToCamelCase(value) {
+  if (_.isArray(value)) {
+    return _.map(value, obj => _.mapKeys(obj, (v, k) => _.camelCase(k)));
+  }
+  return _.mapKeys(value, (v, k) => _.camelCase(k));
+}
