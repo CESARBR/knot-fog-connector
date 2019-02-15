513592815990cf93a37787cacea9d8b3d651550b
Ramon Ribeiro <rhpr@cesar.org.br>
2019-02-15 17:22:10 -0300

    Convert data from snake_case to camelCase

--- src/interactors/PublishData.js
+++ src/interactors/PublishData.js
@@ -1,10 +1,39 @@
+import convertToCamelCase from 'util/camelCase';
+
 class PublishData {
-  constructor(cloudConnector) {
+  constructor(deviceStore, cloudConnector) {
+    this.deviceStore = deviceStore;
     this.cloudConnector = cloudConnector;
   }
 
+  async convertData(id, data) {
+    const devices = await this.deviceStore.list();
+    const device = devices.find(dev => dev.id === id);
+    const dataToSend = convertToCamelCase(data);
+    dataToSend.sensorId = Number(dataToSend.sensorId);
+
+    const schema = device.schema.find(value => value.sensorId === dataToSend.sensorId);
+
+    switch (schema.valueType) {
+      case 1:
+      case 2:
+        dataToSend.value = Number(dataToSend.value);
+        break;
+      case 3:
+        if (dataToSend.value === 'true') {
+          dataToSend.value = true;
+        } else if (dataToSend.value === 'false') {
+          dataToSend.value = false;
+        }
+        break;
+      default:
+        break;
+    }
+    return dataToSend;
+  }
+
   async execute(id, data) {
-    await this.cloudConnector.publishData(id, [data]);
+    await this.cloudConnector.publishData(id, [await this.convertData(id, data)]);
   }
 }
 
