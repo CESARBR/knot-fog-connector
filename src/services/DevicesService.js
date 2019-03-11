3bbf6054844e7faafd4e1789e5543be2ae5205e6
João Neto <joao.alexandre@cesar.org.br>
2019-03-11 10:47:12 -0300

    Remove unused method from DevicesService
    Change-Id: I450f66dbb9fe5b5c06580305944fc3466afe72d6

--- src/services/DevicesService.js
+++ src/services/DevicesService.js
@@ -11,10 +11,6 @@ class DevicesService {
   async updateChanges(device) {
     await this.updateChangesInteractor.execute(device);
   }
-
-  async publishData(deviceId, data) {
-    await this.publishDataInteractor.execute(deviceId, data);
-  }
 }
 
 export default DevicesService;
