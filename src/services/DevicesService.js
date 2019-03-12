50ab64191ff948f03f7c55abbadea3633c4f30ea
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2019-03-12 23:13:23 +0000

    Revert "Start polling instead of running LoadDevices"
    This reverts commit 668ebd464a6e2a24adc9383c48f2099025be1af1.
    
    Reason for revert: the initial subscription performed by LoadDevices is
    required to receive fog devices events.
    
    Change-Id: Iebf2e4e684faca545210c997afe005163da0ba24

--- src/services/DevicesService.js
+++ src/services/DevicesService.js
@@ -1,11 +1,16 @@
 class DevicesService {
-  constructor(updateDevicesInteractor, updateChangesInteractor) {
+  constructor(updateDevicesInteractor, loadDevicesInteractor, updateChangesInteractor) {
     this.updateDevicesInteractor = updateDevicesInteractor;
+    this.loadDevicesInteractor = loadDevicesInteractor;
     this.updateChangesInteractor = updateChangesInteractor;
   }
 
   async update() {
     await this.updateDevicesInteractor.execute();
+  }
+
+  async load() {
+    await this.loadDevicesInteractor.execute();
   }
 
   async updateChanges(device) {
