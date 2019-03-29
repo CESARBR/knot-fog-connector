ff5ddfaf671d3cea90ab6aeb59ff0d2eb0fa6bf9
João Neto <joao.alexandre@cesar.org.br>
2019-03-29 16:58:55 -0300

    Add cloud connection handler
    Change-Id: I035cab7dbf1524ca1f86498dfede5e3fab93c12b

--- src/network/CloudConnectorFactory.js
+++ src/network/CloudConnectorFactory.js
@@ -1,16 +1,16 @@
 import KnotCloudConnector from '@cesarbr/knot-fog-connector-knot-cloud';
 import logger from 'util/logger';
 
-class ConnectorFactory {
-  static create(type, settings) {
-    switch (type) {
+class CloudConnectorFactory {
+  static create(cloudType, cloud) {
+    switch (cloudType) {
       case 'KNOT_CLOUD':
         logger.info('KNoT Cloud Connector selected');
-        return new KnotCloudConnector(settings);
+        return new KnotCloudConnector(cloud);
       default:
         throw Error('Unknown cloud');
     }
   }
 }
 
-export default ConnectorFactory;
+export default CloudConnectorFactory;
