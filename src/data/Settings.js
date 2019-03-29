91d96f1be9be3cb09896572bc54d585fa4d45734
João Neto <joao.alexandre@cesar.org.br>
2019-03-29 16:31:05 -0300

    Add settings factory
    Change-Id: If584190306202c0762a96326b7826bd4f1b9e7e1

--- src/data/Settings.js
+++ src/data/Settings.js
@@ -1,26 +1,9 @@
-import Credentials from 'entities/Credentials';
-import Address from 'entities/Address';
-import config from 'config';
-
 class Settings {
-  async getFogCredentials() {
-    return new Credentials(config.get('fog.uuid'), config.get('fog.token'));
-  }
-
-  async getFogAddress() {
-    return new Address(config.get('fog.host'), config.get('fog.port'));
-  }
-
-  async getCloudSettings() {
-    return config.get('cloud');
-  }
-
-  async getCloudType() {
-    return config.get('cloudType');
-  }
-
-  async getRunAs() {
-    return config.get('runAs');
+  constructor(fog, cloudType, cloud, runAs) {
+    this.fog = fog;
+    this.cloudType = cloudType;
+    this.cloud = cloud;
+    this.runAs = runAs;
   }
 }
 
