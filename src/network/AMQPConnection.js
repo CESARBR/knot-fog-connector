67a4e136d5ec5386dc3cf32f8e1d908b89139174
João Neto <joao.alexandre@cesar.org.br>
2019-04-01 14:34:32 -0300

    Add AMQP capabilities
    Change-Id: I0d643db8c52a026ca513b9e9253c59a6e1ffe748

---
+++ src/network/AMQPConnection.js
@@ -0,0 +1,32 @@
+import amqplib from 'amqplib';
+
+class AMQPConnection {
+  constructor(settings) {
+    this.url = `amqp://${settings.hostname}:${settings.port}`;
+  }
+
+  async start() {
+    const connection = await amqplib.connect(this.url);
+    this.channel = await connection.createChannel();
+    return this.channel;
+  }
+
+  async send(topic, key, message) {
+    await this.channel.assertExchange(topic, 'topic', { durable: true });
+    await this.channel.publish(
+      topic,
+      key,
+      Buffer.from(JSON.stringify(message)),
+      { persistent: true },
+    );
+  }
+
+  async onMessage(topic, key, callback) {
+    await this.channel.assertExchange(topic, 'topic', { durable: true });
+    const { queue } = await this.channel.assertQueue(`${topic}-messages`, { durable: true });
+    await this.channel.bindQueue(queue, topic, key);
+    await this.channel.consume(queue, callback);
+  }
+}
+
+export default AMQPConnection;
