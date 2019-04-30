e1ce17acc2656c94834719be1dc1eb98569bafbb
Leonardo Melo <lcbm@cesar.school>
2019-04-30 19:03:19 -0300

    Re-enable FIWARE support
    Update CloudConnectorFactory to load FIWARE settings.
    
    Change-Id: I6ab09903c7c6b53053d98f75d173de52eaa7943b

--- src/network/CloudConnectorFactory.js
+++ src/network/CloudConnectorFactory.js
@@ -1,4 +1,6 @@
 import KnotCloudConnector from '@cesarbr/knot-fog-connector-knot-cloud';
+import FiwareConnector from '@cesarbr/knot-fog-connector-fiware';
+
 import logger from 'util/logger';
 
 class CloudConnectorFactory {
@@ -7,6 +9,9 @@ class CloudConnectorFactory {
       case 'KNOT_CLOUD':
         logger.info('KNoT Cloud Connector selected');
         return new KnotCloudConnector(settings);
+      case 'FIWARE':
+        logger.info('Fiware connector selected');
+        return new FiwareConnector(settings);
       default:
         throw Error('Unknown cloud');
     }
