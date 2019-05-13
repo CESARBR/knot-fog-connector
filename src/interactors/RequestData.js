b56edbfefd8e71ca32eb87c4d90a9ee32fd319b6
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2019-05-13 19:14:43 -0300

    Fix mapping of request data payload
    onDataRequested's callback expects an array of integers and not objects that
    can be converted to camel case, reason why it was converting the array to an
    empty object. This patch maps each integer into an object in the format
    expected by the fog, e.g. { sensor_id: 1 }
    
    Change-Id: I39fb3d8d922680a607d7a60fe8546b45a95c3d83

--- src/interactors/RequestData.js
+++ src/interactors/RequestData.js
@@ -1,12 +1,14 @@
-import convertToSnakeCase from 'util/snakeCase';
-
 class RequestData {
   constructor(fogConnector) {
     this.fogConnector = fogConnector;
   }
 
   async execute(id, sensorIds) {
-    await this.fogConnector.requestData(id, convertToSnakeCase(sensorIds));
+    await this.fogConnector.requestData(id, this.mapToFogSensorIds(sensorIds));
+  }
+
+  mapToFogSensorIds(sensorIds) {
+    return sensorIds.map(sensorId => ({ sensor_id: sensorId }));
   }
 }
 
