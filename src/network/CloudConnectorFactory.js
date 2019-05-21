e58364943a0d6fe7e5125d5bef75d25d7a20fb17
Leonardo Melo <lcbm@cesar.school>
2019-05-21 18:07:58 -0300

    Re-enable FIWARE
    Change-Id: Ide4fdf12c907e0af3890e05bbde0aa9c0692c843

--- src/network/CloudConnectorFactory.js
+++ src/network/CloudConnectorFactory.js
@@ -1,9 +1,11 @@
 import KNoTCloudConnector from '@cesarbr/knot-fog-connector-knot-cloud';
+import FiwareConnector from '@cesarbr/knot-fog-connector-fiware';
 
 class CloudConnectorFactory {
   static create(type, settings) {
     const connectors = {
       KNOT_CLOUD: KNoTCloudConnector,
+      FIWARE: FiwareConnector,
     };
     return new connectors[type](settings);
   }
