75ee0f5554e7fd35e53e51d481567389a79ec1ff
carolmariaab <cmab1995@gmail.com>
2019-06-25 10:36:46 -0300

    Integrate to Mindsphere cloud

--- src/network/CloudConnectorFactory.js
+++ src/network/CloudConnectorFactory.js
@@ -1,9 +1,11 @@
 import KNoTCloudConnector from '@cesarbr/knot-fog-connector-knot-cloud';
+import MindsphereCloudConnector from '@isitics/knot-fog-connector-mindsphere';
 
 class CloudConnectorFactory {
   static create(type, settings) {
     const connectors = {
       KNOT_CLOUD: KNoTCloudConnector,
+      MINDSPHERE_CLOUD: MindsphereCloudConnector,
     };
     return new connectors[type](settings);
   }
