e71d2cf31721ca41a8cb3fd83dc09ec4f715b113
João Neto <joao.alexandre@cesar.org.br>
2020-04-20 19:30:32 -0300

    settings: Rename from RabbitMQ to Fog
    Considering the fog service is being re-enabled and an user token is
    now necessary to operate on it, this patch changes the settings concept
    from RabbitMQ to Fog.

--- src/data/Settings.js
+++ src/data/Settings.js
@@ -1,9 +1,9 @@
 class Settings {
-  constructor(cloudType, cloud, runAs, rabbitMQ) {
+  constructor(cloudType, cloud, fog, runAs) {
     this.cloudType = cloudType;
     this.cloud = cloud;
+    this.fog = fog;
     this.runAs = runAs;
-    this.rabbitMQ = rabbitMQ;
   }
 }
 
