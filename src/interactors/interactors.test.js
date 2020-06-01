b4d6e01bbe83e8ebe4d883c5f86aea7e61f04155
Esdras Júnior <eblsj@cesar.org.br>
2020-06-01 10:26:53 -0300

    tests: Add tests for updateSchema interactor

--- src/interactors/interactors.test.js
+++ src/interactors/interactors.test.js
@@ -1,12 +1,22 @@
 import Cloud, * as cloudMocks from '@cesarbr/knot-fog-connector-knot-cloud';
 import RegisterDevice from './RegisterDevice';
 import UnregisterDevice from './UnregisterDevice';
+import UpdateSchema from './UpdateSchema';
 
 jest.mock('@cesarbr/knot-fog-connector-knot-cloud');
 
 const mockThing = {
   id: 'abcdef1234568790',
   name: 'my-device',
+  schema: [
+    {
+      sensorId: 0,
+      typeId: 65521,
+      valueType: 3,
+      unit: 0,
+      name: 'bool-sensor',
+    },
+  ],
 };
 
 describe('Interactors', () => {
@@ -43,5 +53,21 @@ describe('Interactors', () => {
     const unregisterDevice = new UnregisterDevice(cloud);
     await unregisterDevice.execute(mockThing);
     expect(cloudMocks.mockRemoveDevice).toHaveBeenCalled();
+  });
+
+  test("should execute correctly when updating a thing's schema on cloud", async () => {
+    const cloud = new Cloud();
+    const updateSchema = new UpdateSchema(cloud);
+    await updateSchema.execute(mockThing);
+    expect(cloudMocks.mockUpdateSchema).toHaveBeenCalled();
+  });
+
+  test("should execute without errors when updating a thing's schema on cloud fails", async () => {
+    const cloud = new Cloud({
+      updateSchemaErr: "fail to update thing's schema",
+    });
+    const updateSchema = new UpdateSchema(cloud);
+    await updateSchema.execute(mockThing);
+    expect(cloudMocks.mockUpdateSchema).toHaveBeenCalled();
   });
 });
